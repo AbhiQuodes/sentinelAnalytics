ffe222d9d2ef0f75b988a66683f00e7d9c6fea54
AbhiQuodes <bhartiabhinav618@gmail.com>
2025-01-10 17:39:16 +0530

    1st  commit

--- src/App.js
+++ src/App.js
@@ -1,74 +1,69 @@
-import { useEffect, useRef, useState } from 'react';
-import './App.css';
-import FormContainer from './component/FormContainer';
-import appLogoImg from './images/appLogo.png'
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+import FormContainer from "./component/FormContainer";
+import appLogoImg from "./images/appLogo.png";
 function App() {
-   const displayWidth = window.innerWidth;
-    const pageRef = useRef();
-    const [deferredPrompt ,setDeferredPrompt] = useState(null);
-    const [showInstallBox,setShowInstallBox]=useState(false);
-    useEffect(()=>{
-      window.addEventListener("beforeinstallprompt",(e)=>{
-        e.preventDefault();
-        setDeferredPrompt(e);
-      });
-    },[]);
-  
-    const handleInstallDisplay=()=>{
-      if(showInstallBox)
-      {
-        console.log("scrolling")
-        setShowInstallBox(false);
-        pageRef.current.removeEventListener("touchstart",handleInstallDisplay);
-  
-      }
-  
+  const displayWidth = window.innerWidth;
+  const pageRef = useRef();
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [showInstallBox, setShowInstallBox] = useState(false);
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault();
+      setDeferredPrompt(e);
+    });
+  }, []);
+
+  const handleInstallDisplay = () => {
+    if (showInstallBox) {
+      console.log("scrolling");
+      setShowInstallBox(false);
+      pageRef.current.removeEventListener("touchstart", handleInstallDisplay);
     }
-    useEffect(()=>{
-  
-      pageRef.current.addEventListener("touchstart",handleInstallDisplay);
-      if(displayWidth<= 600)
-        {
-          setTimeout(()=>{
-            setShowInstallBox(true)
-          }, 2000); 
-        }
-        return ()=>{
-          clearTimeout(()=>{
-            setShowInstallBox(true)
-          });
-          pageRef.current.removeEventListener("touchstart",handleInstallDisplay);
-  
-        }     
-      },[]);
-  
-    const handleInstall =()=>{
-      if(deferredPrompt){
-        deferredPrompt.prompt();
-        deferredPrompt.userChoice.then((choiceResult)=>{
-          if(choiceResult.outcome === "accepted")
-          {
-            console.log("User accepted the install prompt");
-          }
-          setDeferredPrompt(null);
-        });
-      }
+  };
+  useEffect(() => {
+    pageRef.current.addEventListener("touchstart", handleInstallDisplay);
+    if (displayWidth <= 600) {
+      setTimeout(() => {
+        setShowInstallBox(true);
+      }, 2000);
+    }
+    return () => {
+      clearTimeout(() => {
+        setShowInstallBox(true);
+      });
+      pageRef.current.removeEventListener("touchstart", handleInstallDisplay);
     };
+  }, []);
+
+  const handleInstall = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the install prompt");
+        }
+        setDeferredPrompt(null);
+      });
+    }
+  };
   return (
     <div className="App">
-    <FormContainer></FormContainer>
-        <div>
-            {
-              deferredPrompt && showInstallBox &&(
-                
-                <div className="install-box">
-                  <img className="install-img" src={appLogoImg} alt='app-logo'></img>
-                  <h1 className="install-title"> Aryan Enterprises</h1>
-                <button className="install-btn" onClick={handleInstall}> Install App</button>
-                </div>
-              )
-            }
+      <div onClick={handleInstallDisplay} ref={pageRef}>
+        <FormContainer></FormContainer>
+      </div>
+      <div>
+        {deferredPrompt && showInstallBox && (
+          <div className="install-box">
+            <img className="install-img" src={appLogoImg} alt="app-logo"></img>
+            <h1 className="install-title"> Aryan Enterprises</h1>
+            <button className="install-btn" onClick={handleInstall}>
+              {" "}
+              Install App
+            </button>
           </div>
+        )}
+      </div>
     </div>
   );
 }
